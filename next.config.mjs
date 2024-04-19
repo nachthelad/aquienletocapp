c857ee5bf5a3f56ce8dbc957631fc2359646572e
Nacho <nachthelad@gmail.com>
2024-04-19 19:05:48 -0300

    Added PWA

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+});
+
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
 };
 
-export default nextConfig;
+export default withPWA({ nextConfig });
